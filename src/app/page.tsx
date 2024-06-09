3e459c46ed9b0f891764b5115f36fe85e5c086c5
unic <2445942195@qq.com>
2024-06-10 01:24:14 +0800

    ♻️ Refactor components and enhance layout
    Migrate components from "page.tsx" to a newly created "components.tsx" for code organization and restructuring, enhancing readability and maintainability, and facilitating future component reuse. Additionally, streamlined the search input box and chat input components in "page.tsx" to improve UI and user interaction.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import { cn } from "@/lib/util";
-import messagesjson from "../../dev/data/message.json";
-import { NavbarTitle } from "@/components/navbartitle";
-import { showTimestamp, ChatBubble } from "@/components/chatbubble";
-import { ThemeController } from "@/components/themecontroller";
-import { ExpandableButton } from "@/components/expandableBbutton";
-import SearchTextBox from "@/components/searchtextbox";
-import ChatInput from "@/components/chatinput";
+
+import {
+  messagesjson,
+  NavbarTitle,
+  ChatBubble,
+  ThemeController,
+  ExpandableButton,
+  SearchTextBox,
+  ChatInput,
+} from "@/components/components";
 
 const bgshow = false;
 
@@ -18,26 +20,15 @@ const chatColor = bgshow ? "bg-blue-300" : null;
 const chatbgColor = bgshow ? "bg-cyan-300" : null;
 const footerColor = bgshow ? "bg-green-300" : null;
 
+let theme = "light";
 const user_name = "Anakin";
 
+const handleEvent = (query: string) => {
+  console.log("event changed:", query);
+  // Handle the input change logic
+};
+
 export default function Home() {
-  let theme = "light";
-
-  const handleSearch = (query: string) => {
-    console.log("Search query:", query);
-    // Handle search logic here
-  };
-
-  const handleSend = (message: string) => {
-    console.log("Message sent:", message);
-    // Handle the message sending logic
-  };
-
-  const handleChange = (query: string) => {
-    console.log("Input changed:", query);
-    // Handle the input change logic
-  };
-
   return (
     <main className="app-layout flex flex-col h-screen hero">
       {/* Header Navbar */}
@@ -68,10 +59,7 @@ export default function Home() {
           </div>
           {/* Navbar End */}
           <div className="flex flex-row flex-none navbar-end">
-            <div className="z-10">
-              {/* Search */}
-              <SearchTextBox onSearch={handleSearch} />
-            </div>
+            <SearchTextBox onSearch={handleEvent} />
             {/* Theme controller */}
             <ThemeController
               key={"themeContoller"}
@@ -96,42 +84,20 @@ export default function Home() {
           chatbgColor
         )}
       >
-        <div>
-          <div
-            className={cn("flex flex-col min-h-fit min-w-full p-2", chatColor)}
-          >
-            {/* Message Receiver */}
-            <div id="message-list">
-              {messagesjson.map((message, index) => (
-                <ChatBubble
-                  key={message.id}
-                  user={user_name}
-                  message={message}
-                  lastMessage={messagesjson[index - 1] || null}
-                  spanSeconds={300}
-                />
-              ))}
-            </div>
-
-            {/* Message Sender */}
-            {/* <div className="chat chat-end">
-              <div className="chat-image avatar">
-                <div className="w-10 rounded-full">
-                  <Image
-                    width={10}
-                    height={10}
-                    alt="Tailwind CSS chat bubble component"
-                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                  />
-                </div>
-              </div>
-              <div className="chat-header">
-                Anakin
-                <time className="text-xs opacity-50">12:46</time>
-              </div>
-              <div className="chat-bubble">I hate you!</div>
-              <div className="chat-footer opacity-50">Seen at 12:46</div>
-            </div> */}
+        <div
+          className={cn("flex flex-col min-h-fit min-w-full p-2", chatColor)}
+        >
+          {/* Messages */}
+          <div id="message-list">
+            {messagesjson.map((message, index) => (
+              <ChatBubble
+                key={message.id}
+                user={user_name}
+                message={message}
+                lastMessage={messagesjson[index - 1] || null}
+                spanSeconds={300}
+              />
+            ))}
           </div>
         </div>
       </div>
@@ -143,7 +109,7 @@ export default function Home() {
           footerColor
         )}
       >
-        <ChatInput onSend={handleSend} onChange={handleChange} />
+        <ChatInput onSend={handleEvent} onChange={handleEvent} />
       </div>
     </main>
   );
---
+++ src/components/components.tsx
@@ -0,0 +1,17 @@
+import messagesjson from "../../dev/data/message.json";
+import NavbarTitle from "@/components/navbartitle";
+import ChatBubble from "@/components/chatbubble";
+import ThemeController from "@/components/themecontroller";
+import ExpandableButton from "@/components/expandableBbutton";
+import SearchTextBox from "@/components/searchtextbox";
+import ChatInput from "@/components/chatinput";
+
+export {
+  messagesjson,
+  NavbarTitle,
+  ChatBubble,
+  ThemeController,
+  ExpandableButton,
+  SearchTextBox,
+  ChatInput,
+};
